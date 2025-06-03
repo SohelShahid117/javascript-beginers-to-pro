01bbfabbafcaad193b1ebe402156e48ec761a723
SohelShahid117 <sohelshahid09@gmail.com>
2025-06-03 22:54:11 +0600

    78. Data Types in JS (strings, numbers, booleans, undefined, null)  part 2

--- day-1/js/app.js
+++ day-1/js/app.js
@@ -18,7 +18,7 @@ console.log(age);
 age = 40;
 console.log(age);
 
-//77. Data Types in JS (strings, numbers, booleans, undefined, null) part 1
+//77,78. Data Types in JS (strings, numbers, booleans, undefined, null) part 1 & part 2
 
 //string
 let greeting = "hello";
@@ -31,28 +31,51 @@ console.log(templateLiteral);
 //number
 let num1 = 33; //integer
 let price = 55.99; //float
-let bignumber = 1e7; //exponential notation
+let bigNumber = 1e7; //exponential notation
 console.log(num1);
 console.log(price);
-console.log(bignumber);
+console.log(bigNumber);
 
-//what is the difference between undefined & null?
-//when use undefined & when use null in javascript
+/*what is the difference between undefined & null?
+when use undefined & when use null in javascript */
 
-let x; //undefined:A variable has been declared but has not been assigned a value.
-//undefined — Something is missing or not yet assigned
-
+let x;
+/*undefined:A variable has been declared but has not been assigned a value.
+undefined — Something is missing or not yet assigned
+memory dokol ache but kono value nai
+*/
 console.log(x);
 console.log(typeof x);
 
 function test() {}
 console.log("function is " + test()); // undefined
 
-let y = null; //null:Represents an intentional absence of value;No value (intentionally empty)
-//Use null when you want to deliberately(-ইচ্ছাকৃতভাবে) clear or reset a value.
+let y = null;
+/*null:Represents an intentional absence of value;No value (intentionally empty)
+Use null when you want to deliberately(-ইচ্ছাকৃতভাবে) clear or reset a value.
+null-is intentionally absence of any types of values
+*/
+// y = 5;
+// y = "hi";
+// y = true;
+// y = { a: 5 };
+// y = [4, 5, 6];
 
 console.log(y);
-console.log(typeof y);
+// console.log(y[0]);
+// console.log(typeof y[0]); //number
+console.log(typeof y); //object
 
-console.log(undefined == null); //equality
-console.log(undefined === null); //Strict equality
+console.log(undefined == null); //equality--->true
+console.log(undefined === null); //Strict equality-->false
+
+//79. Basic Operators: Arithmetic Operators
+
+const a = 10;
+const b = 3;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a ** b);
