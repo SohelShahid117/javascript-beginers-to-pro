dfc368f57a7ebc458756e9a7ead4a1b2f22e57ad
SohelShahid117 <sohelshahid09@gmail.com>
2025-06-04 13:12:50 +0600

    82. Declaring strings and string literals

--- day-2/day2.js
+++ day-2/day2.js
@@ -1,1 +1,22 @@
 console.log("day2");
+//82. Declaring strings and string literals
+let text1 = "hello sohel";
+let text2 = "hello Aadil";
+let text3 = `hello Abdullah`;
+console.log(text1);
+console.log(text2);
+console.log(text3);
+
+let p1 = "Aadil";
+let p2 = "Shahid";
+
+let greeting = `hello ${p1},"How r u?"`;
+console.log(greeting);
+let fun = 'javascript is "fun"';
+console.log(fun);
+
+let poem = "poem is \nmultiline";
+let poem2 = `poem 
+is multiline`;
+console.log(poem);
+console.log(poem2);
