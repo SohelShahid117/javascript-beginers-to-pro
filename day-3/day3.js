4d717b9bd7270ef82a88406a4fceaf135e4ce6fa
SohelShahid117 <sohelshahid09@gmail.com>
2025-06-05 23:38:40 +0600

    89. Boolean values: true and false done

--- day-3/day3.js
+++ day-3/day3.js
@@ -1,1 +1,14 @@
 //89. Boolean values: true and false
+
+// let isJavascriptFun = true;
+let isJavascriptFun = 1;
+console.log(isJavascriptFun);
+
+let isMathHard = false; //0,null,undefined,empty string ""
+console.log(isMathHard);
+
+let num1 = 8;
+let num2 = 5;
+console.log(num1 > num2);
+
+//90. Conditional statements: if, else if, else
