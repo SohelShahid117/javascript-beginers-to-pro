afed8269b06ef8d4109ccaf126eb4cabc81cb01f
SohelShahid117 <sohelshahid09@gmail.com>
2025-06-08 23:28:46 +0600

    100. Combining, slicing, and joining arrays: concat(), slice(), join() done

--- day-4/day4.js
+++ day-4/day4.js
@@ -17,3 +17,18 @@ fruits.map((f) => {
 });
 
 //100. Combining, slicing, and joining arrays: concat(), slice(), join()
+
+const arr1 = [1, 2];
+const arr2 = [3, 4];
+// const combinedArr = arr1.concat(arr2);
+const combinedArr = arr2.concat(arr1);
+console.log(combinedArr);
+
+const slicedArr = combinedArr.slice(1, 3);
+console.log(slicedArr);
+
+const words = ["hello", "javascript"];
+const sentence = words.join("     ");
+console.log(sentence);
+
+//101. Introduction to JavaScript Object
