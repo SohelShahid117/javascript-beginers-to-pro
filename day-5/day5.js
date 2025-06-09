884cfa8afd0ca420909db216aedbe50198239af3
SohelShahid117 <sohelshahid09@gmail.com>
2025-06-09 10:33:20 +0600

    107. Looping through arrays using  and  done

--- day-5/day5.js
+++ day-5/day5.js
@@ -1,1 +1,13 @@
 //107. Looping through arrays using `for` and `for...of`
+
+const fruits = ["apple", "banana", "orange"];
+for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
+
+console.log("using for...of loop: ");
+for (const x of fruits) {
+  console.log(x);
+}
+
+//108. Controlling Loops with break and continue with Practical Project
