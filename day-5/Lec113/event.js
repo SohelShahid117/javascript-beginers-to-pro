0a15dd16d599623595fe40245bd9e00844f5c0c6
SohelShahid117 <sohelshahid09@gmail.com>
2025-06-09 23:00:11 +0600

    114. JavaScript Event Handling: (hover event) done

--- day-5/Lec113/event.js
+++ day-5/Lec113/event.js
@@ -1,4 +1,4 @@
-//113. JavaScript Event Handling: (click event
+//113. JavaScript Event Handling: (click event)
 const clickButton = document.getElementById("clickBtn");
 // console.log(clickButton);
 
@@ -9,3 +9,17 @@ clickButton.addEventListener("click", () => {
   alert("the button is clicked");
   message.innerText = "the button is clicked";
 });
+
+//114. JavaScript Event Handling: (hover event)
+const hoverDiv = document.getElementById("hoverDiv");
+
+hoverDiv.addEventListener("mouseover", () => {
+  hoverDiv.style.backgroundColor = "lightGreen";
+  hoverDiv.innerText = "u r hovering";
+});
+hoverDiv.addEventListener("mouseout", () => {
+  hoverDiv.style.backgroundColor = "lightblue";
+  hoverDiv.innerText = "Hover Me";
+});
+
+//115. Handling Keyboard Events (keydown and keyup)
