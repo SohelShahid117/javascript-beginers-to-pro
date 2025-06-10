d6a28d655716384ce50ed4668ca98218d05ebd45
SohelShahid117 <sohelshahid09@gmail.com>
2025-06-10 14:42:09 +0600

    119. Selecting elements: getElementById(), querySelector(), querySelectorAll() 10 min done

---
+++ day-6/js/app.js
@@ -0,0 +1,9 @@
+//119. Selecting elements: getElementById(), querySelector(), querySelectorAll()
+console.log("hi");
+
+const header = document.getElementById("header");
+console.log(header);
+console.log(header.innerText);
+header.innerText = "enjoy javascript";
+
+//10 min  done
