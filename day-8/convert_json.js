d22331d2986e6c0ad127ad61f5cac150458c8342
SohelShahid117 <sohelshahid09@gmail.com>
2025-06-13 01:30:18 +0600

    132. Parsing JSON Strings Back into Objects with JSON.parse() done

--- day-8/convert_json.js
+++ day-8/convert_json.js
@@ -1,4 +1,6 @@
 //131. Converting JavaScript Objects to JSON Using JSON.stringify()
+
+//JSON.stringify()--->convert obj to JSON
 const user = {
   name: "sohel",
   age: 35,
@@ -15,4 +17,11 @@ const convertToJSONDataLimited = JSON.stringify(user, ["name", "age"]);
 console.log("converting object to JSON data : ", convertToJSONData);
 console.log("specific JSON data store to DB : ", convertToJSONDataLimited);
 
-//132. Parsing JSON Strings Back into Objects with JSON.parse(
+//132. Parsing JSON Strings Back into Objects with JSON.parse()
+
+//JSON.parse()--------->convert JSON data to Obj
+
+const convertJSONtoObj = JSON.parse(convertToJSONData);
+console.log("JSON to object data is : ", convertJSONtoObj);
+
+//133. Error Handling in JavaScript (Syntax, Runtime & Logical Errors)
